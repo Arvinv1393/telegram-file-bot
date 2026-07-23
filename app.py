cb443afe734084c002e218b3fb5134f9533bed61
Arvinv1393 <arrvinn14@gmail.com>
2026-07-23 20:20:08 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Maveraec Telegram Bot is running successfully."
+
+if name == "__main__":
+    app.run(host="0.0.0.0", port=10000)
